4c678c49504ea7433739e4b88b7383f1f0980beb
coolade1012 <coolade1012@gmail.com>
2015-03-29 02:30:47 -0400

    removed dummy file, renamed testcherry1.py to startserver.py, Altered dexter.js, index.html, and startserver.py to follow step 8 of the cherrypy tutorial

--- public/js/dexter.js
+++ public/js/dexter.js
@@ -3,126 +3,141 @@
         //and sorting of information into the appropraite places on the page
         //and sorting of information into the appropraite places on the page
         $(document).ready(function () {
-           //search function
-           $("#search").click(function (e) {
-               $.post("/generator", {
-                       "length": $("input[name='length']").val()
-                   })
-                   .done(function (string) {
-                       $("#the-string").show();
-                       $("#the-string input").val(string);
-                   });
-               e.preventDefault();
-           });
+            //search function
+            $("#generate-result").click(function (e) {
+                $.post("/generator", {
+                        "identifier": $("input[name='identifier']").val()
+                    })
+                    .done(function (string) {
+                        $("#the-result").show();
+          --              $("#the-display").val(string);
+                    });
+                e.preventDefault();
+            });
 
-           $("#delete-string").click(function (e) {
-               $.ajax({
-                       type: "DELETE",
-                       url: "/generator"
-                   })
-                   .done(function () {
-                       $("#the-string").hide();
-                   });
-               e.preventDefault();
-           });
-           
-           //Expanding and Collapsing Button info
-           $('.tour-caption-Basic').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-           $('.tour-caption-Dex').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-           $('.tour-caption-Stats').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-           $('.tour-caption-Evolution').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-           $('.tour-caption-Type').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-           $('.tour-caption').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-           $('.tour-caption-Breeding').click(function () {
-               if (this.value === 'collapse') {
-                   // if it's open close it
-                   open = false;
-                   this.value = 'expand';
-                   $(this).next("div.Pokemon-Info").hide("slow");
-               } else {
-                   // if it's close open it
-                   open = true;
-                   this.value = 'collapse';
-                   $(this).siblings("[value='collapse']").click();
-                   $(this).next("div.Pokemon-Info").show("slow");
-               }
-           });
-       });
+            $("#replace-result").click(function (e) {
+                $.ajax({
+                        type: "PUT",
+                        url: "/generator",
+                        data: {
+                            "another_result": $("#the-result").val()
+                        }
+                    })
+                    .done(function () {
+                        alert("Replaced!");
+                    });
+                e.preventDefault();
+            });
+
+            $("#delete-result").click(function (e) {
+                $.ajax({
+                        type: "DELETE",
+                        url: "/generator"
+                    })
+                    .done(function () {
+                        $("#the-result").hide();
+                    });
+                e.preventDefault();
+            });
+
+
+            //Expanding and Collapsing Button info
+            $('.tour-caption-Basic').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+            $('.tour-caption-Dex').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+            $('.tour-caption-Stats').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+            $('.tour-caption-Evolution').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+            $('.tour-caption-Type').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+            $('.tour-caption').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+            $('.tour-caption-Breeding').click(function () {
+                if (this.value === 'collapse') {
+                    // if it's open close it
+                    open = false;
+                    this.value = 'expand';
+                    $(this).next("div.Pokemon-Info").hide("slow");
+                } else {
+                    // if it's close open it
+                    open = true;
+                    this.value = 'collapse';
+                    $(this).siblings("[value='collapse']").click();
+                    $(this).next("div.Pokemon-Info").show("slow");
+                }
+            });
+        });
